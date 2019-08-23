681c5ec9f90556d03556c08b8dfc5493bf244a3d
bcauchon <bcauchon@gmail.com>
2019-08-23 11:18:59 -0700

    Add validations to animal attributes

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,10 @@
 class Animal < ApplicationRecord
+  validates :animal_type, presence: true
+  validates :name, presence: true
+  validates :breed, presence: true
+  validates :sex, presence: true
+  validates :age, presence: true
+  validates :age, numericality: { greater_than: 0 }
+  validates :weight, presence: true
+  validates :weight, numericality: { greater_than: 0 }
 end
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Animal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :animal_type }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :breed }
+  it { should validate_presence_of :sex }
+  it { should validate_presence_of :age }
+  it { should validate_presence_of :weight }
+  it { should validate_numericality_of(:age).is_greater_than(0) }
+  it { should validate_numericality_of(:weight).is_greater_than(0) }
 end
