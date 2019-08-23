fb5b58a8d9fe911cfd665e9b3aa695872b0ff6bd
bcauchon <bcauchon@gmail.com>
2019-08-23 13:34:26 -0700

    Add specs for remaining animal requests

--- spec/requests/animals_spec.rb
+++ spec/requests/animals_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe "Animals", type: :request do
       get animals_path
     end
 
-    it "returns a success response" do
+    it "returns a success status" do
       expect(response).to have_http_status(:success)
     end
 
@@ -54,7 +54,9 @@ RSpec.describe "Animals", type: :request do
       it "renders a JSON response with the new animal" do
         expect(response).to have_http_status(:created)
         expect(response.content_type).to eq('application/json')
-        expect(response.location).to eq(animal_url(Animal.last))
+        animal = Animal.find(JSON.parse(response.body)["id"])
+        expect(response.location).to eq(animal_url(animal))
+        expect_animal_to_match_params(animal, valid_params)
       end
     end
 
@@ -63,10 +65,113 @@ RSpec.describe "Animals", type: :request do
         post '/animals', params: invalid_params
       end
 
-      it "renders a JSON response with errors for the new animal" do
+      it "returns a unprocessable_entity status" do
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to eq('application/json')
       end
     end
+  end
+
+  describe "GET /animals/:id" do
+    let (:animal) { FactoryBot.create(:animal) }
+
+    context "with valid :id" do
+      before do
+        get "/animals/#{animal.id}"
+      end
+
+      it "returns a success status" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "renders a JSON response with the animal" do
+        expect(response.content_type).to eq('application/json')
+        expect_json_response_to_match_animal(response, animal)
+      end
+
+    end
+
+    context "with invalid :id" do
+      it "returns a not_found status" do
+        get "/animals/foo"
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+  end
+
+  describe "PUT /animals/:id" do
+    let (:animal) { FactoryBot.create(:animal) }
+
+    context "with valid :id and params" do
+      before do
+        put "/animals/#{animal.id}", params: valid_params
+        animal.reload
+      end
+
+      it "returns a success status" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "updates the requested animal" do
+        expect_animal_to_match_params(animal, valid_params)
+      end
+
+      it "renders a JSON response with the animal" do
+        expect(response.content_type).to eq('application/json')
+        expect_json_response_to_match_animal(response, animal)
+      end
+    end
+
+    context "with valid :id and invalid params" do
+      it "returns a unprocessable_entity status" do
+        put "/animals/#{animal.id}", params: invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context "with invalid :id" do
+      it "returns a not_found status" do
+        put "/animals/foo", params: valid_params
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+  end
+
+  describe "DELETE /animals/:id" do
+    let (:animal) { FactoryBot.create(:animal) }
+
+    context "with valid :id" do
+      it "destroys the requested animal" do
+        delete "/animals/#{animal.id}"
+        get "/animals/#{animal.id}"
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+    context "with invalid :id" do
+      it "returns a not_found status" do
+        delete "/animals/foo"
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+  end
+
+  # Helper methods
+  def expect_json_response_to_match_animal(response, animal)
+    response_body = JSON.parse(response.body)
+    expect(response_body["id"]).to eq(animal.id)
+    expect(response_body["animal_type"]).to eq(animal.animal_type)
+    expect(response_body["name"]).to eq(animal.name)
+    expect(response_body["breed"]).to eq(animal.breed)
+    expect(response_body["sex"]).to eq(animal.sex)
+    expect(response_body["age"].to_f).to eq(animal.age)
+    expect(response_body["weight"].to_f).to eq(animal.weight)
+  end
+
+  def expect_animal_to_match_params(animal, params)
+    expect(animal.animal_type).to eq(params[:animal_type])
+    expect(animal.name).to eq(params[:name])
+    expect(animal.breed).to eq(params[:breed])
+    expect(animal.sex).to eq(params[:sex])
+    expect(animal.age).to eq(params[:age])
+    expect(animal.weight).to eq(params[:weight])
   end
 end
