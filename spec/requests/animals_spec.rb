b684203ebc842a0596f5c48aa06635d75a6bf03a
bcauchon <bcauchon@gmail.com>
2019-08-23 17:00:22 -0700

    Add tests for search endpoint

--- spec/requests/animals_spec.rb
+++ spec/requests/animals_spec.rb
@@ -166,6 +166,156 @@ RSpec.describe "Animals", type: :request do
     end
   end
 
+  describe "GET /search" do
+    before do
+    end
+
+    context "with valid params" do
+      it "returns only dogs if animal_type=dog is passed" do
+        FactoryBot.create_list(:dog, 10)
+        FactoryBot.create_list(:cat, 20)
+        get "/search", params: { animal_type: "dog" }
+        response_body = JSON.parse(response.body)
+        expect(response_body.size).to eq(10)
+        response_body.each do |animal|
+          expect(animal["animal_type"]).to eq("Dog")
+        end
+      end
+
+      it "returns only cats if animal_type=cats is passed" do
+        FactoryBot.create_list(:dog, 10)
+        FactoryBot.create_list(:cat, 20)
+        get "/search", params: { animal_type: "cats" }
+        response_body = JSON.parse(response.body)
+        expect(response_body.size).to eq(20)
+        response_body.each do |obj|
+          expect(obj["animal_type"]).to eq("Cat")
+        end
+      end
+
+      it "returns only retrievers if breed=retriever is passed" do
+        animal = FactoryBot.create(:dog, breed: "Retriever")
+        FactoryBot.create(:dog, breed: "Shepherd")
+        get "/search", params: { breed: "retriever" }
+        response_body = JSON.parse(response.body)
+        response_body.each do |obj|
+          expect(obj["breed"].include?("Retriever")).to eq(true)
+        end
+        animal_found = false
+        response_body.each do |obj|
+          if obj["id"] == animal.id
+            animal_found = true
+            break
+          end
+        end
+        expect(animal_found).to eq(true)
+      end
+
+      it "returns only male animals if sex=male is passed" do
+        animal = FactoryBot.create(:dog, sex: "Male")
+        FactoryBot.create(:dog, sex: "Cat")
+        get "/search", params: { sex: "male" }
+        response_body = JSON.parse(response.body)
+        response_body.each do |obj|
+          expect(obj["sex"]).to eq("Male")
+        end
+        animal_found = false
+        response_body.each do |obj|
+          if obj["id"] == animal.id
+            animal_found = true
+            break
+          end
+        end
+        expect(animal_found).to eq(true)
+      end
+
+      it "returns only animals over a certain age if min_age is given" do
+        animal = FactoryBot.create(:dog, age: 10)
+        FactoryBot.create(:dog, age: 5)
+        get "/search", params: { min_age: 6 }
+        response_body = JSON.parse(response.body)
+        response_body.each do |obj|
+          expect(obj["age"] >= 6).to be(true)
+        end
+        animal_found = false
+        response_body.each do |obj|
+          if obj["id"] == animal.id
+            animal_found = true
+            break
+          end
+        end
+        expect(animal_found).to eq(true)
+      end
+
+      it "returns only animals under a certain age if max_age is given" do
+        animal = FactoryBot.create(:dog, age: 5)
+        FactoryBot.create(:dog, age: 10)
+        get "/search", params: { max_age: 6 }
+        response_body = JSON.parse(response.body)
+        response_body.each do |obj|
+          expect(obj["age"] <= 6).to be(true)
+        end
+        animal_found = false
+        response_body.each do |obj|
+          if obj["id"] == animal.id
+            animal_found = true
+            break
+          end
+        end
+        expect(animal_found).to eq(true)
+      end
+
+      it "returns only animals within a certain age range if min_age and max_age are given" do
+        animal = FactoryBot.create(:dog, age: 5)
+        FactoryBot.create(:dog, age: 10)
+        FactoryBot.create(:cat, age: 1)
+        get "/search", params: { min_age: 2, max_age: 9 }
+        response_body = JSON.parse(response.body)
+        response_body.each do |obj|
+          age = obj["age"]
+          expect(2 <= age).to be(true)
+          expect(age <= 9).to be(true)
+        end
+        animal_found = false
+        response_body.each do |obj|
+          if obj["id"] == animal.id
+            animal_found = true
+            break
+          end
+        end
+        expect(animal_found).to eq(true)
+      end
+
+      it "returns only female cats if animal_type=cats and sex=female are passed" do
+        animal = FactoryBot.create(:cat, sex: "Female")
+        FactoryBot.create(:dog, sex: "Female")
+        FactoryBot.create(:cat, sex: "Male")
+        get "/search", params: { animal_type: "cats", sex: "female" }
+        response_body = JSON.parse(response.body)
+        response_body.each do |obj|
+          expect(obj["animal_type"]).to eq("Cat")
+          expect(obj["sex"]).to eq("Female")
+        end
+        animal_found = false
+        response_body.each do |obj|
+          if obj["id"] == animal.id
+            animal_found = true
+            break
+          end
+        end
+        expect(animal_found).to eq(true)
+      end
+    end
+
+    context "with invalid params" do
+      it "returns a JSON response with status unprocessable_entity" do
+        get "/search", params: { foo: "Dog" }
+        expect(response.content_type).to eq('application/json')
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
   # Helper methods
   def expect_json_response_to_match_animal(response, animal)
     response_body = JSON.parse(response.body)
